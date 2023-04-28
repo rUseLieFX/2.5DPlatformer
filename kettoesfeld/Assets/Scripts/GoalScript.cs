185674658af849bcca11880f9431a3e564fda6b3
rUseLieFX <felix@zs.h4it.net>
2023-04-28 12:17:36 +0200

    Pályák készítése, változtatása.
    E1M0 pálya színezése, hogy a játékos megszokja a különböző színeket.
    E1M3 pálya elkészítése.
    E1M4 pálya elkészítése, nincs befejezve, csak a könnyű út.
    
    GoalScript-nek egy Editor script készítése, bár semmi haszna nincsen jelenleg.

--- kettoesfeld/Assets/Scripts/GoalScript.cs
+++ kettoesfeld/Assets/Scripts/GoalScript.cs
@@ -5,12 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class GoalScript : MonoBehaviour
 {
+    bool loaded = false;
     [SerializeField] string sceneToLoad;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Pályatöltés.");
-        
-        SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+        if (!loaded)
+        {
+            Debug.Log("Pályatöltés.");
+            SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+            loaded = true;
+        }
     }
 }
